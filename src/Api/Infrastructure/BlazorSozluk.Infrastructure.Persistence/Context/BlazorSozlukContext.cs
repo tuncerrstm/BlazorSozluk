9bb779f54ee73faae1e4338106ee49af323993b0
tuncerrstm <tuncerrstm@gmail.com>
2022-04-24 23:46:24 +0300

    Upload : Created Entities

---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Context/BlazorSozlukContext.cs
@@ -0,0 +1,61 @@
+﻿using BlazorSozluk.Api.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Infrastructure.Persistence.Context
+{
+    public  class BlazorSozlukContext : DbContext
+    {
+        public const string DEFAULT_SCHEMA = "dbo";
+
+        public BlazorSozlukContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Entry> Entries { get; set; }
+
+        public DbSet<EntryVote> EntryVotes { get; set; }
+        public DbSet<EntryFavorite> EntryFavorites { get; set; }
+
+        public DbSet<EntryComment> EntryComments { get; set; }
+        public DbSet<EntryCommentVote> EntryCommentVotes { get; set; }
+        public DbSet<EntryCommentFavorite> EntryCommentFavorites { get; set; }
+
+        public DbSet<EmailConfirmation> EmailConfirmations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public override int SaveChanges()
+        {
+            //
+            return base.SaveChanges();
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            //
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            //
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            //
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
