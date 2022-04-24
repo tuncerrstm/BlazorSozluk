36d6ab4a513424449b3a12b3ed6b9dd0f97cb869
tuncerrstm <tuncerrstm@gmail.com>
2022-04-25 01:00:53 +0300

    Update Entities

--- src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Context/BlazorSozlukContext.cs
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Context/BlazorSozlukContext.cs
@@ -36,26 +36,44 @@ namespace BlazorSozluk.Infrastructure.Persistence.Context
 
         public override int SaveChanges()
         {
-            //
+            OnBeforeSave();
             return base.SaveChanges();
         }
 
         public override int SaveChanges(bool acceptAllChangesOnSuccess)
         {
-            //
+            OnBeforeSave();
             return base.SaveChanges(acceptAllChangesOnSuccess);
         }
 
         public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
         {
-            //
+            OnBeforeSave();
             return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
         }
 
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
-            //
+            OnBeforeSave();
             return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void OnBeforeSave()
+        {
+            var addedEntities = ChangeTracker.Entries()
+                                      .Where(i => i.State == EntityState.Added)
+                                      .Select(i => (BaseEntity)i.Entity);
+
+            PrepareAddedEntities(addedEntities);
+        }
+
+        private void PrepareAddedEntities(IEnumerable<BaseEntity> entities)
+        {
+            foreach (var entity in entities)
+            {
+                if(entity.CreateDate == DateTime.MinValue)
+                   entity.CreateDate = DateTime.Now;
+            }
         }
     }
 }
